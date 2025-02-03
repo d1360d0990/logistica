46d1e25c6f19e39897f0b674a5e014a185890656
Diego Orlando Diaz <diego.d0990@gmail.com>
2025-02-02 23:12:15 -0300

    realizando cambios para pruebas con el front

--- Models/logisticModel.js
+++ Models/logisticModel.js
@@ -1,32 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const logisticSchema = new mongoose.Schema({
-
-      destino: {
-        type: String,
-        required: true,
-      },
-      fechaCreacion: {
-        type: Date,
-        default: Date.now
-      },
-      fechaEntrega: {
-        type: Date,
-      },
-      estado: {
-        type: String,
-        enum: ['Pendiente', 'En tránsito', 'Entregado'],
-        default: 'Pendiente', // Estado inicial
-        required: true
-      },
-      detalles: {
-        type: String,
-        required: true,
-      }
-},
-    {
-        timestamps: true,
-    }
+const logisticSchema = new mongoose.Schema(
+  {
+    remitente: {
+      type: String,
+      required: true,
+    },
+    destinatario: {
+      type: String,
+      required: true,
+    },
+    origen: {
+      type: String,
+      required: true,
+    },
+    destino: {
+      type: String,
+      required: true,
+    },
+    fechaCreacion: {
+      type: Date,
+      default: Date.now,
+    },
+    fechaEntrega: {
+      type: Date,
+    },
+    estado: {
+      type: String,
+      enum: ["Pendiente", "En tránsito", "Entregado"],
+      default: "Pendiente", // Estado inicial
+      required: true,
+    },
+    detalles: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const modelLogistic = mongoose.model("Logistica", logisticSchema);
